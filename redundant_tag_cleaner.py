9ceaf4fb70c65d792a22f2ef61f097111d042415
Nine <filenine.mail+git@gmail.com>
2023-10-24 18:40:37 -0500

    Add cleanup for disctotal tags

--- redundant_tag_cleaner.py
+++ redundant_tag_cleaner.py
@@ -4,6 +4,8 @@
 """
 This program iterates through the audio files in a folder.
 
+The code assumes that the given list of files are a single disc of an album.
+
 If the "artist" tag is the same for all of the files and the value of the
 "albumartist" tag is equal to the value of the "artist" tag, it deletes the
 "albumartist" tag for all audio files in the folder.
@@ -39,32 +41,46 @@ def clean_albumartist(files):
     Input: "files" - a list of files to be iterated over and cleaned up
     """
     first_artist = mutagen.File(files[0])["artist"]
-    album_has_single_artist = true # True by default, to handle single-track albums
+    album_has_single_artist = True # True by default, to handle single-track albums
 
     # "If the artist tag is the same for all of them"
     for file in files:
         if mutagen.File(file)["artist"] != first_artist:
-            album_has_single_artist = false
+            album_has_single_artist = False
             break # We already know the album has multiple artists - no need to keep looping
     
     # Check if the value of the albumartist tag is the same as the artist
     # If they're the same, delete the albumartist tag
     # This assumes every track has the same albumartist value
     if album_has_single_artist:
-        if mutagen.File(files[0]).has("albumartist"):
+        if "albumartist" in mutagen.File(files[0]):
             if mutagen.File(files[0])["albumartist"] == first_artist:
                 for file in files:
                     metadata = mutagen.File(file)
                     metadata.pop("albumartist", None)
                     metadata.save()
 
-def clean_disctotal(files)
+def clean_disctotal(files):
     """
     Removes the "discnumber", "disctotal" and "totaldiscs" tags if either
     the "disctotal" or "totaldiscs" tags are equal to 1
 
     Input: "files" - a list of files to be iterated over and cleaned up
     """
+    for file in files:
+        single_disc = False
+        metadata = mutagen.File(file)
+        if "disctotal" in metadata:
+            if metadata["disctotal"][0] == u"1":
+                single_disc = True
+                metadata.pop("disctotal")
+        if "totaldiscs" in metadata:
+            if metadata["totaldiscs"][0] == u"1":
+                single_disc = True
+                metadata.pop("totaldiscs")
+        if single_disc == True:
+            if "discnumber" in metadata:
+                metadata.pop("discnumber")
 
 def main():
     files = sys.argv[1:]
